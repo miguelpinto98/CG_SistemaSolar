97ea6de7ab2578865c4e44ec7f21c1f1f95ae463
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-04-02 15:28:38 +0100

    floats

--- xmlteste/xmlteste/teste.cpp
+++ xmlteste/xmlteste/teste.cpp
@@ -14,14 +14,61 @@ void teste(XMLElement* grupo){
 
 			//transformações para um grupo
 			for (XMLElement* transformacao = grupo->FirstChildElement(); (strcmp(transformacao->Value(), "modelos")!=0); transformacao = transformacao->NextSiblingElement()) {
+				
+				//translacao
 				if(strcmp(transformacao->Value(), "translacao")==0) {
-					printf("%s - %s, %s, %s\n", transformacao->Value(), transformacao->Attribute("X"), transformacao->Attribute("Y"), transformacao->Attribute("Z"));}
- 
+					float transX, transY, transZ;
+
+					if(transformacao->Attribute("X") == NULL) transX=0;
+					else transX= stof(transformacao->Attribute("X"));
+
+					if(transformacao->Attribute("Y") == NULL) transY=0;
+					else transY= stof(transformacao->Attribute("Y"));
+
+					if(transformacao->Attribute("Z") == NULL) transZ=0;
+					else transZ= stof(transformacao->Attribute("Z"));
+
+					printf("%s - %f, %f, %f\n", transformacao->Value(), transX, transY, transZ);	
+				}
+
+
+
+				//rotacao
 				if(strcmp(transformacao->Value(), "rotacao")==0) {
-					printf("%s - %s, %s, %s, %s \n", transformacao->Value(), transformacao->Attribute("angulo"), transformacao->Attribute("eixoX"), transformacao->Attribute("eixoY"), transformacao->Attribute("eixoZ"));}
+					float rotAng, rotEixoX, rotEixoY, rotEixoZ;
+
+					if(transformacao->Attribute("angulo") == NULL) rotAng=0;
+					else rotAng= stof(transformacao->Attribute("angulo"));
+
+					if(transformacao->Attribute("eixoX") == NULL) rotEixoX=0;
+					else rotEixoX= stof(transformacao->Attribute("eixoX"));
+
+					if(transformacao->Attribute("eixoY") == NULL) rotEixoY=0;
+					else rotEixoY= stof(transformacao->Attribute("eixoY"));
+
+					if(transformacao->Attribute("eixoZ") == NULL) rotEixoZ=0;
+					else rotEixoZ= stof(transformacao->Attribute("eixoZ"));
+
+					printf("%s - %f, %f, %f, %f \n", transformacao->Value(),rotAng, rotEixoX, rotEixoY, rotEixoZ);
+				}
 		 
+
+
+				//escala
 				if(strcmp(transformacao->Value(), "escala")==0) {
-					printf("%s\n", transformacao->Value());}
+					float escX, escY, escZ;
+
+					if(transformacao->Attribute("angulo") == NULL) escX=0;
+					else escX= stof(transformacao->Attribute("angulo"));
+
+					if(transformacao->Attribute("eixoX") == NULL) escY=0;
+					else escY= stof(transformacao->Attribute("eixoX"));
+
+					if(transformacao->Attribute("eixoY") == NULL) escZ=0;
+					else escZ= stof(transformacao->Attribute("eixoY"));
+
+					printf("%s - %f, %f, %f\n", transformacao->Value(), escX, escY, escZ);
+				}
 			}
 	
 
@@ -48,7 +95,7 @@ void readXML(string fxml) {
 }
 
 int main(int argc, char **argv) {
-	string xmlmotor="teste63.xml"; //BARRIGAS, apenas mudas aqui o nome do ficheiro e botas para correr :D
+	string xmlmotor="outro.xml"; //BARRIGAS, apenas mudas aqui o nome do ficheiro e botas para correr :D
 	readXML(xmlmotor);
 	return 1;
 }
